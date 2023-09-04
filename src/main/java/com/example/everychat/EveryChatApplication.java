67c9b8215fdf5d3bfa6f45b2ecccfb2e49adc034
y00913-laptop <y00913@naver.com>
2023-09-04 16:43:06 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/example/everychat/EveryChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.everychat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EveryChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/everychat/EveryChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.everychat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
