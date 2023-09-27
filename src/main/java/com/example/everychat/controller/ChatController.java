aa9c5de7eab2554008f3a6daf570e46b73881c5e
y00913 <ayt0913@gmail.com>
2023-09-27 16:34:26 +0900

    feat: ChatDto 추가

--- src/main/java/com/example/everychat/controller/ChatController.java
+++ src/main/java/com/example/everychat/controller/ChatController.java
@@ -1,18 +1,22 @@
 package com.example.everychat.controller;
 
+import com.example.everychat.dto.ChatDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/chat")
+@RequestMapping
 @RequiredArgsConstructor
 public class ChatController {
 
-    @GetMapping("/test")
-    public String test(){
-        return "test";
+    private final SimpMessageSendingOperations simpMessageSendingOperations;
+
+    @MessageMapping("/test")
+    public void message(ChatDto message){
+        simpMessageSendingOperations.convertAndSend("/topic/" + message.getChannelId(), message);
     }
 
 }
---
+++ src/main/java/com/example/everychat/dto/ChatDto.java
@@ -0,0 +1,17 @@
+package com.example.everychat.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatDto {
+    private String type;
+    private String sender;
+    private String channelId;
+    private Object data;
+}
