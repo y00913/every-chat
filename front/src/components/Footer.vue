027be4634e25ad214fcd8b65d697477a1307c0e2
y00913 <ayt0913@gmail.com>
2023-11-01 14:34:49 +0900

    feat: front 하단 url 변경

--- front/src/components/Footer.vue
+++ front/src/components/Footer.vue
@@ -1,19 +1,13 @@
 <template>
     <footer>
         <p style="font-weight: bold; margin-top: 4.5vw;">
-          made by <a v-bind:href="`https://velog.io/@y00913`" target="_blank" style="text-decoration: none;">y00913</a>
+          made by <a v-bind:href="`https://github.com/y00913`" target="_blank" style="text-decoration: none;">y00913</a>
         </p>
     </footer>
 </template>
 
 <script>
-export default {
-    name: 'App',
-    data() {
-        return {
-        }
-    }
-}
+export default {}
 </script>
 
 <style>
