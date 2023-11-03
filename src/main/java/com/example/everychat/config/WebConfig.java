a7a3df3f30b6e291a3dfe556d7774b36752fed9c
y00913 <ayt0913@gmail.com>
2023-11-03 18:00:54 +0900

    fix: cors 오류 처리

--- src/main/java/com/example/everychat/config/WebConfig.java
+++ src/main/java/com/example/everychat/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http//everychat.kro.kr")
+                .allowedOrigins("http://localhost:3000", "http//everychat.kro.kr:8082")
                 .allowedMethods("*")
                 .exposedHeaders("*");
     }
