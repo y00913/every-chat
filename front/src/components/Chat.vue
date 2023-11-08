c0f878c52749cc361128eb2c161543b8a5905f74
y00913 <ayt0913@gmail.com>
2023-11-08 22:02:24 +0900

    feat: front outgoing 주기 변경

--- front/src/components/Chat.vue
+++ front/src/components/Chat.vue
@@ -159,7 +159,7 @@ export default {
       var options = { debug: false, protocols: ['v11.stomp', 'v12.stomp'] };
       this.stompClient = Stomp.over(socket, options);
 
-      this.stompClient.heartbeat.outgoing = 300000;
+      this.stompClient.heartbeat.outgoing = 60000;
       this.stompClient.heartbeat.incoming = 0;
 
       console.log('소켓 연결 시도');
