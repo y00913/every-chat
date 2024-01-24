284505db8da32cb9451976eb433faf8934127478
y00913-laptop <y00913@naver.com>
2024-01-24 17:59:07 +0900

    feat: aop 추가

--- src/main/java/com/example/everychat/EveryChatApplication.java
+++ src/main/java/com/example/everychat/EveryChatApplication.java
@@ -2,7 +2,9 @@ package com.example.everychat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy
 @SpringBootApplication
 public class EveryChatApplication {
 
