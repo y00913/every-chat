b2d86e45302ff956d1a6cb6f68174afa7474534b
yutae <ayt0913@gmail.com>
2024-07-04 13:14:46 +0900

    Update Header.vue

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -1,5 +1,6 @@
 <template>
     <header style="margin-top:5vh; margin-bottom: 1vh;">
+        <meta name="google-site-verification" content="-xWUUSw1ZEbfMJNUwLlAQCWxJimAXfqyZdncyrf9094" />
         <title>every chat</title>
         <link rel="shortcut icon" type="image/x-icon" href="https://cdn-icons-png.flaticon.com/512/5962/5962500.png">
         <h1>{{ title }}</h1>
