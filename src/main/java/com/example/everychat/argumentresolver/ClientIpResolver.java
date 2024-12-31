3ac07c9f07de9d32cf69f81ea5bb6d742687332b
y00913 <ayt0913@gmail.com>
2024-12-31 23:46:27 +0900

    refactor: ip 추출 로직 리팩토링

--- src/main/java/com/example/everychat/argumentresolver/ClientIpResolver.java
+++ src/main/java/com/example/everychat/argumentresolver/ClientIpResolver.java
@@ -10,20 +10,32 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+import java.lang.reflect.Field;
+
 @Component
 public class ClientIpResolver implements HandlerMethodArgumentResolver {
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
-        return parameter.getParameterType().isAnnotationPresent(ClientIp.class);
+        return Object.class.isAssignableFrom(parameter.getParameterType());
     }
 
     @Override
-    public Object resolveArgument(MethodParameter parameter,
-                                  ModelAndViewContainer mavContainer,
-                                  NativeWebRequest webRequest,
-                                  WebDataBinderFactory binderFactory) {
-        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
-        return ClientUtil.getIp(request);
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
+        String clientIp = ClientUtil.getIp(request);
+
+        Class<?> parameterType = parameter.getParameterType();
+        Object instance = parameterType.getDeclaredConstructor().newInstance();
+
+        for (Field field : parameterType.getDeclaredFields()) {
+            if (field.isAnnotationPresent(ClientIp.class) && field.getType().equals(String.class)) {
+                field.setAccessible(true);
+                field.set(instance, clientIp);
+            }
+        }
+
+        return instance;
     }
 }
