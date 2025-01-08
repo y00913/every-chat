f2f5d7a9e39c554cf0e1acb54ec1be22b06e080d
yutae <ayt0913@gmail.com>
2025-01-08 16:13:12 +0900

    fix: @ClientIp 수정

---
+++ src/main/java/com/example/everychat/aop/ClientIpAop.java
@@ -0,0 +1,47 @@
+package com.example.everychat.aop;
+
+import com.example.everychat.annotation.ClientIp;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.lang.reflect.Field;
+
+@Aspect
+@Slf4j
+@Component
+public class ClientIpAop {
+
+    @Around("@annotation(com.example.everychat.annotation.ClientIp)")
+    public Object injectClientIp(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object[] args = joinPoint.getArgs();
+
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (attributes == null) {
+            log.warn("No request context available.");
+            return joinPoint.proceed(args);
+        }
+
+        HttpServletRequest request = attributes.getRequest();
+        String clientIp = request.getRemoteAddr();
+
+        for (Object arg : args) {
+            if (arg == null) continue;
+
+            Field[] fields = arg.getClass().getDeclaredFields();
+            for (Field field : fields) {
+                if (field.isAnnotationPresent(ClientIp.class)) {
+                    field.setAccessible(true);
+                    field.set(arg, clientIp);
+                }
+            }
+        }
+
+        return joinPoint.proceed(args);
+    }
+}
