e084b5c28e728465a2bb0de8285c8c0570138cd5
yutae <ayt0913@gmail.com>
2025-01-09 15:20:28 +0900

    style: 배경색 수정

--- front/src/App.vue
+++ front/src/App.vue
@@ -33,7 +33,7 @@ export default {
 <style>
 #app {
   font-family: 'mabi';
-  background-color: #F2F2F2;
+  background-color: #F8F9FA;
   min-width: 100vw;
   min-height: 100vh;
   isolation: isolate;
